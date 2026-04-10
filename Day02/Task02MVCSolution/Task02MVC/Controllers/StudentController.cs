2055e6fcc578f33dc1b6c2060c962dc565208c58
YoussefAyman77 <youssefstemer@gmail.com>
2026-04-10 22:11:52 +0200

    Task2Added

---
+++ Day02/Task02MVCSolution/Task02MVC/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task02MVC.Models;
+
+namespace Task02MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentBL StuBl = new StudentBL();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ShowAll()
+        {
+            List<Student> StuList = StuBl.GetAll();
+            return View("ShowAll", StuList);
+        }
+
+        public IActionResult ShowByID(int Id)
+        {
+            Student stu = StuBl.GetById(Id);
+            if (stu == null) return NotFound();
+            return View("ShowByID", stu);
+        }
+    }
+}
---
+++ Day02/Task02MVCSolution/Task02MVC/Models/StudentBL.cs
@@ -0,0 +1,17 @@
+﻿using Task02MVC.Data.Contexts;
+
+namespace Task02MVC.Models
+{
+    public class StudentBL : Student
+    {
+        UniDbContext context = new UniDbContext();
+        public List<Student> GetAll()
+        {
+            return context.Students.ToList();
+        }
+        public Student GetById(int id)
+        {
+            return context.Students.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
