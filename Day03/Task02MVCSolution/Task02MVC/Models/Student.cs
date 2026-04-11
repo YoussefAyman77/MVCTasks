a792c8dc541b8098cacf917b035aa5ba2ac2356c
YoussefAyman77 <youssefstemer@gmail.com>
2026-04-11 02:38:25 +0200

    Task03

---
+++ Day03/Task02MVCSolution/Task02MVC/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace Task02MVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+        public List<StuCrsRes> StudCourseResults { get; set; }
+
+    }
+}
